4076b7ba46e06e35809bbd2b282db6ad993d46cc
Stephen Duffy <steve@Stephens-MacBook-Pro.local>
2017-11-12 21:58:07 -0500

    broken adding authors page

--- src/main.js
+++ src/main.js
@@ -1,34 +1,39 @@
 $ = jQuery = require('jquery');
 var React = require('React');
 var Home = require('./components/homePage');
+var Authors = require('./components/authors/authorPage'); 
 var About = require('./components/about/aboutPage');
 var Header = require('./components/common/header');
 
-var App = React.createClass({
-	render: function () {
-		var Child;
+(function() {
+	"use strict";
+	var App = React.createClass({
+		render: function () {
+			var Child;
 
-		switch (this.props.route) {
-			case 'about':
-				Child = About;
-				break;
-			default:
-				Child = Home;
+			switch (this.props.route) {
+				case 'about':
+					Child = About;
+					break;
+				case 'authors': Child = Authors; break;
+				default:
+					Child = Home;
+			}
+
+			return (
+				<div>
+					<Header/>
+					<Child />
+				</div>
+			);
 		}
+	});
 
-		return (
-			<div>
-				<Header/>
-				<Child />
-			</div>
-		);
+	function render() {
+		var route = window.location.hash.substr(1);
+		React.render(<App route={route}/>, document.getElementById('app'));
 	}
-});
 
-function render() {
-	var route = window.location.hash.substr(1);
-	React.render(<App route={route}/>, document.getElementById('app'));
-}
-
-window.addEventListener('hashchange', render);
-render();
+	window.addEventListener('hashchange', render);
+	render();
+})();
